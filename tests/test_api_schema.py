55149a7287e17288cc477f73bf89e974d039d0a0
Lukas RABIER <lukasrabier@hotmail.fr>
2025-10-02 18:40:54 +0200

    [CLEANUP] Remove print.

--- tests/test_api_schema.py
+++ tests/test_api_schema.py
@@ -3,7 +3,6 @@ import schemathesis
 
 base_url = "http://127.0.0.1:8000/"
 app = api.setup()
-print(app.router)
 schema = schemathesis.openapi.from_url(base_url + app.openapi_url)
 schema.config.checks.update(excluded_check_names=["positive_data_acceptance"])
 schema.config.output.sanitization.update(enabled=False)
