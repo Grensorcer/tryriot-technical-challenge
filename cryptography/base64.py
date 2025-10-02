794abacefcf4d59455c21a315a06dec8f4020dbb
Lukas RABIER <lukasrabier@hotmail.fr>
2025-10-02 19:24:33 +0200

    [DOCS] Docstrings.

--- cryptography/base64.py
+++ cryptography/base64.py
@@ -5,12 +5,26 @@ from typing import Any
 
 
 def encode(value: Any)-> bytes:
+    """
+    Encode any json-serializable value to base64 encoded string.
+    Handle string ambiguity with `json.dumps` 
+
+    @param value: any json-serializable value
+    @return: base64 encoded string
+    """
     if not isinstance(value, str):
         value = json.dumps(value, separators=(",", ":"), sort_keys=True)
     return b64encode(value.encode())
 
 
-def decode(value: bytes) -> Any:
+def decode(value: bytes) -> Any | None:
+    """
+    Decode base64 encoded json string.
+    Handle string ambiguity with `json.loads`
+
+    @param value: base64 encoded json string
+    @return: decoded json object or None on failure
+    """
     try:
         res = b64decode(value, validate=True).decode()
     except binascii.Error as err:
--- cryptography/hmac.py
+++ cryptography/hmac.py
@@ -4,6 +4,13 @@ from cryptography import base64
 
 
 def hmac256(payload: Payload, secret: bytes):
+    """
+    Generates a HMAC256 signature string for the given payload and secret. 
+
+    @param payload: The payload to sign.
+    @param secret: The secret key used for signing.
+    @return: A Signature object containing the HMAC256 signature.
+    """
     return Signature(
         signature=HMAC(secret, base64.encode(payload), "sha256").hexdigest()
     )
